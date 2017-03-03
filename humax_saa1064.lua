c4118e0228e41ac8bf75e4656e0bba526a9702b5
Thomas Hirschbuechler <t.hirschbuechler@gmail.com>
2017-03-03 20:32:36 +0100

    added display function for numbers, and raw-charakter display alternating with the memory/register position

--- humax_saa1064.lua
+++ humax_saa1064.lua
@@ -3,6 +3,11 @@ id  = 0
 sda = 1
 scl = 2
 
+timer0 = 0
+timer0_iv = 2000
+timer1 = 1
+timer1_iv = 500
+
 i2c.setup(id, sda, scl, i2c.SLOW)
 
 -- scan for devices
@@ -14,8 +19,7 @@ for i=0,127 do
 end
 -- from: http://www.esp8266.com/viewtopic.php?f=19&t=771#sthash.KvPQiex9.dpuf
 
-
-digits={63, 6, 91, 79, 102, 109, 125,7, 127, 111, 119, 124, 57, 94, 121, 113}
+hex_digits={63, 6, 91, 79, 102, 109, 125,7, 127, 111, 119, 124, 57, 94, 121, 113}
 -- 0 to 9 (1-10), then A to F (11-16)
 -- http://tronixstuff.com/2011/07/21/tutorial-arduino-and-the-nxp-saa1064-4-digit-led-display-driver/
 -- http://www.rapidtables.com/convert/number/binary-to-hex.htm
@@ -40,7 +44,7 @@ function wrdata_all(adr, data1, data2, digits)
 end
 
 function test_args(args)
-    wrdata(0x38,0x00,args,digits[2],digits[3],digits[4],digits[5])
+    wrdata(0x38,0x00,args,hex_digits[2],hex_digits[3],hex_digits[4],hex_digits[5])
     -- 1,2,3,4
 end
 
@@ -48,12 +52,16 @@ function testy(args)
     test_args(0x47)
 end
 
+function clear_disp()
+    wrdata_all(0x38, 0x00, 0x47, 0x00)
+end
+-- light up segments at this memory position, plus 3 followers
 function karak(j)
     wrdata(0x38, 0x00, 0x47, j, j+1, j+2, j+3)
 end
 
-function dance(untils)
-    for i=0,untils do
+function dance(count)
+    for i=0,count do
       wrdata_all(0x38, 0x00, 0x47, i)
       print(i)
       tmr.delay(1000)
@@ -64,4 +72,92 @@ end
 
 function go_dance()
     dance(127)
+end
+
+function intarray(digit)
+    buffer = tostring(digit)
+    len=string.len(buffer)
+    data = {}
+
+    m=digit
+    for i=1,len do
+        if  (m%10>0) then
+          table.insert(data,1,m%10)
+          -- 1 means insert as first element
+        elseif m>9 then
+          -- 10 or bigger means add a "0"
+          table.insert(data,1,0)
+        else
+          -- smaller than 10 means last digit
+          table.insert(data,1,m)
+        end
+
+        m=m/10
+    end
+
+    table.foreachi(data,print)
+
+    return data
+end
+
+function printdigits(number)
+    tmr.stop(timer0)
+    digits = intarray(number)
+    str_digits = {}
+
+    for i=1,table.getn(digits) do
+        table.insert(str_digits,i,hex_digits[digits[i]+1])
+        -- +1 in every case as the arrays (=tables) start at 1, not 0,
+        -- but the "0" digit is at position 1 of hex_digits
+    end
+
+    -- if there are less than 4 digits, fill in blanks
+    for i=table.getn(str_digits),3 do
+        table.insert(str_digits,0x00)
+    end
+    digits = {}
+    -- free the array
+
+    count=table.getn(str_digits)-3
+
+    -- start tmr_digit at i=1
+    tmr_digit(1,count,str_digits)
+
+    tmr.start(timer0)
+
+end
+
+--this cycles through str_digits
+function tmr_digit(i,count, str_digits)
+  tmr.alarm(timer1, timer1_iv, tmr.ALARM_SINGLE, function ()
+      print("tmr1 activated")
+      j=i+1
+      print("i+1",j)
+      print("str_digits[i+1] == ",str_digits[i+1])
+      wrdata(0x38,0x00,0x47,str_digits[i],str_digits[i+1],str_digits[i+2],str_digits[i+3])
+      -- +1 more for every right digit to adress the next one
+      -- if there are only 4 digits, the timer doesn't get used more than once
+
+      -- if i<count start over at the next position
+      if i<count then
+          tmr_digit(i+1,count,str_digits)
+      end
+
+      end)
+end
+
+-- turns segment on at val, puts out val, alternates
+function valinfo(val)
+    clear_disp()
+    tmr.alarm(timer0, timer0_iv, 1, function ()
+        print("tmr0 activated")
+        if value then
+            print("disp 4 characters at the given pos")
+            karak(val)
+        else
+            print("disp the pos")
+            printdigits(val)
+        end
+        value = not value
+    end)
 end
